2e8f3bb81270330477514d9f3fefd9d73c584844
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2020-09-12 14:05:41 +0700

    part7

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -40,18 +40,9 @@ def index(request):
     context = {'latest_question_list': latest_question_list,}
     return HttpResponse(template.render(context, request))
 
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
 
 
 def detail(request, question_id):
