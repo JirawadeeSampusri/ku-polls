66b447159ba6ba38de42455b1931c6fa1dbd95e0
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2020-10-10 12:58:21 +0700

    fix timezone

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'ASIA/BANGKOK'
+TIME_ZONE = config('TIME_ZONE', default= "UTC")
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
